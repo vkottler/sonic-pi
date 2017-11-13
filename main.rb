e5608cb0cd078021347a1ba0c03f27fce4993f35
vkottler <vkottler@wisc.edu>
2017-11-12 21:56:57 -0600

    get even more life out of the two sets of riffs by iterating through all combinations and swapping bass with treble on second run through

--- main.rb
+++ main.rb
@@ -23,35 +23,81 @@ bass_keys = [
 	:Ab2, :Eb3, :Bb2, :F3
 ]
 
-# Bass
-in_thread do
-	loop do
-		for bkey in bass_keys do
-			use_synth :chipbass
-			puts "Using key #{bkey}"
-			for riff in BASS_RIFFS do
-				riff.key = bkey
-				play_melody(riff)
-			end
-		end
-	end
-end
-
-# circle of fifths
 melody_keys = [
 	:C4, :G3, :D4, :A3,
 	:E4, :B3, :Gb4, :Db4,
 	:Ab3, :Eb4, :Bb3, :F4
 ]
 
+# Bass
+in_thread do
+	loop do
+		use_synth :chipbass
+
+		# Play through bass keys
+		for bkey in bass_keys do
+			puts "Using key (bass) #{bkey}"
+			for riff in BASS_RIFFS do
+				riff.key = bkey
+				play_melody(riff)
+			end
+		end
+
+		# Play through melody keys
+		for mkey in melody_keys do
+			puts "Using key (bass) #{mkey}"
+			for riff in BASS_RIFFS do
+				riff.key = mkey
+				play_melody(riff)
+			end
+		end
+	end
+end
+
 # Melody
 loop do
+	use_synth :chiplead
+	
+	# Play through melody keys
 	for mkey in melody_keys do
-		use_synth :chiplead
-		puts "Using key #{mkey}"
+		puts "Using key (meldoy) #{mkey}"
 		for riff in MELODY_RIFFS do
 			riff.key = mkey
 			play_melody(riff)
 		end
 	end
+
+	# Play through bass keys
+	for bkey in bass_keys do
+		puts "Using key (meldoy) #{bkey}"
+		for riff in MELODY_RIFFS do
+			riff.key = bkey
+			play_melody(riff)
+		end
+	end
 end
+
+# Percussion
+#in_thread do
+#	loop do
+#		sleep 32
+#		j = 0
+#		while j < 8 do
+#			sample :bd_haus, cutoff: 70, amp: 2.0
+#			sleep 4
+#			j += 1
+#		end
+#		j = 0
+#		while j < 16 do
+#			sample :bd_haus, cutoff: 70, amp: 2.0
+#			sleep 2
+#			j += 1
+#		end
+#		j = 0
+#		while j < 8 do
+#			sample :bd_haus, cutoff: 70, amp: 2.0
+#			sleep 4
+#			j += 1
+#		end
+#	end
+#end
